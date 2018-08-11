973982190180c09ba03c5f8588bca37cb38426be
GavinYeapMY <gavinyeap.my@gmail.com>
2018-08-11 22:46:37 +0800

    Submitted Files

---
+++ Desktop/ExploratoryDataAnalysisProject2/Plot2.R
@@ -0,0 +1,17 @@
+NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+totalNEI <- NEI[fips=='24510', lapply(.SD, sum, na.rm = TRUE)
+                , .SDcols = c("Emissions")
+                , by = year]
+
+SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
+NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
+
+
+png(filename='plot2.png')
+
+barplot(totalNEI[, Emissions]
+        , names = totalNEI[, year]
+        , xlab = "Years", ylab = "Emissions"
+        , main = "Emissions over the Years")
+
+dev.off()
---
+++ Desktop/ExploratoryDataAnalysisProject2/Plot3.R
@@ -0,0 +1,19 @@
+# Subset NEI data by Baltimore
+
+library(ggplot2)
+library(data.table)
+
+
+baltimoreNEI <- NEI[fips=="24510",]
+
+
+
+ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
+  geom_bar(stat="identity") +
+  facet_grid(.~type,scales = "free",space="free") + 
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+
+ggsave(file='plot3.png')
+
+dev.off()
